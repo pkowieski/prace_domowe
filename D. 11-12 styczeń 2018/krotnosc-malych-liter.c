d45529e43b0ff388f4d9fbedd02d89fbfccb0fef
pkowieski <kowieski.pawel@gmial.com>
2018-02-08 10:58:35 +0100

    Prace domowe aktualizacja

---
+++ D. 11-12 styczeń 2018/krotnosc-malych-liter.c
@@ -0,0 +1,47 @@
+// Pawel Kowieski
+// Informatyka grupa 2
+	
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+
+bool czy_znak(char znak)
+{
+	if((znak>='a'&&znak<='z')||(znak>='A'&&znak<='Z'))
+		return true;
+	else
+		return false;
+}
+
+void zlicz(char* ciag, int* histogram)
+{
+	int i=0;
+	while(ciag[i])
+	{
+		if(czy_znak(ciag[i]));
+		{
+			if(ciag[i]>='a')
+				histogram[ciag[i]-'a']++;
+			else
+				histogram[ciag[i]-'A']++;
+		}
+		i++;
+	}
+}
+
+void wypisz(int* histogram)
+{
+	int i;
+	for(i=0;i<26;i++){
+		printf("%c - %d \n",'a'+i, histogram[i]);
+	}
+}
+
+int main()
+{
+	char* ciag="Ala ma kota, a kot ma Ale.";
+	int histogram[26]={0};
+	zlicz(ciag,histogram);
+	wypisz(histogram);
+
+}
---
+++ E. 25-26 styczeń 2018/NWD-rek.c
@@ -0,0 +1,25 @@
+// Pawel Kowieski
+// Informatyka grupa 2
+	
+#include <stdio.h>
+
+int nwd(int a,int b)
+{
+	if(b==0){
+		return a;
+	}
+	else{
+    		return nwd(b,a%b);
+	}
+}
+
+int main()
+{
+	int a,b;
+	printf("Program obliczajacy największy wspólny dzielnik dwóch liczb\n");
+	printf("Podaj a = "); scanf("%d", &a);
+	printf("Podaj b = "); scanf("%d", &b);
+	printf("NWD = %d\n",nwd(a,b));
+	return 0;
+}
+
